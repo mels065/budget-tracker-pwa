57adaca56bd7590e644cdc6cadc36bee6a98d078
Brandon Mellus <mellusbrandon@gmail.com>
2021-10-09 11:45:37 -0700

    Define static cache name and assets

---
+++ public/sw.js
@@ -0,0 +1,10 @@
+STATIC_CACHE_NAME = 'budget-tracker-static-cache-v1';
+STATIC_CACHE_ASSETS = [
+    '/',
+    'index.html',
+    'manifest.json',
+    'index.js',
+    'styles.css',
+    'icons/icon-192x192.png',
+    'icons/icon-192x192.png'
+];
